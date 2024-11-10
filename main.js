d9a3e5933fb00d4ad6031bcbd41dd6c5d17a8abb
deividali <damingan123@gmail.com>
2024-11-10 09:20:46 -0500

    Cambios en pagina y estilo

--- main.js
+++ main.js
@@ -18,3 +18,14 @@ document.getElementById('form')
       alert(JSON.stringify(err));
     });
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.fixed-action-btn');
+  var instances = M.FloatingActionButton.init(elems, options);
+});
+
+// Or with jQuery
+
+$(document).ready(function(){
+  $('.fixed-action-btn').floatingActionButton();
+});
